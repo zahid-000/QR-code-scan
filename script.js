d7073eb744686ac0926fb6f263d297de67b3cca4
zahid-000 <136980388+zahid-000@users.noreply.github.com>
2024-11-19 14:26:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js file
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        // Redirect to the URL contained in the QR code
+        window.location.href = decodeText;
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
